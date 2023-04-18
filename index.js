b4c1127cfbd9a8e293fb25cceb1700824afffcf5
ctalv <122413805+ctalv@users.noreply.github.com>
2023-04-18 12:25:13 -0500

    base readme generated

--- index.js
+++ index.js
@@ -3,13 +3,58 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}.md`, data, (err) =>
+    err ? console.log(err) : console.log('Successfully created README file!')
+    )
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    const generateREADME = (...questions) =>
+    `# Project Title
+
+    ## Description
+    
+    
+    ## Table of Contents 
+    
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    - [License](#license)
+
+    
+    ## Installation
+    
+    
+    ## Usage
+    
+ 
+    ## Contributing
+    
+
+    ## Tests
+
+    
+    ## Questions
+
+
+    ## License
+    
+    `
+
+    writeToFile(generateREADME, 'cool')
+}
 
 // Function call to initialize app
 init();
